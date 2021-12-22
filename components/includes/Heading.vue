467a21dc6b17158d1e64da8160119d8d4d41b128
anthony1861996 <vaa1861@gmail.com>
2021-12-22 08:50:55 +0300

    create app

---
+++ components/includes/Heading.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="container">
+    <header class="d-flex justify-content-start px-3 py-3 border border-info">
+      <ul class="nav nav-pills">
+          <h1>Новости</h1>
+      </ul>
+    </header>
+  </div>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Heading />
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+import Heading from "@/components/includes/Heading";
+
+export default {
+  components: {
+    Heading,
+  },
+};
+</script>
